a0fa5e09cf177da2d9cea0c34ffbd0665ad143e0
deniloss <danil.sitnik.work@gmail.com>
2022-06-13 13:32:29 +0300

    fix: Исправлена иконка удаления фильма из сохраненных

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -5,8 +5,15 @@ import bookmark from '../../../images/bookmark.svg'
 
 // todo После создания БД с фото удалить эту строчку
 import image from '../../../images/movie-example.png';
+import deleteIcon from '../../../images/deleteIcon.svg';
+import {useLocation} from "react-router";
 
 const MoviesCard = (props) => {
+
+  const { pathname } = useLocation();
+
+  let Icon = pathname !== '/saved-movies' ? bookmark : deleteIcon;
+
   return (
     <li className={cl.card}>
       <div className={cl.card__header}>
@@ -14,7 +21,7 @@ const MoviesCard = (props) => {
           <h3 className={cl.card__title}>{props.title}</h3>
           <p className={cl.card__time}>{props.time}</p>
         </div>
-        <button className={cl.card__save}><img src={bookmark} alt="bookmark to save"/></button>
+        <button className={cl.card__save}><img src={Icon} alt="bookmark"/></button>
       </div>
 
       <img className={cl.card__image} src={image} alt={props.desc}/>
