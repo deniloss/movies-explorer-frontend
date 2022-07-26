6016b00d22ebdc354cb59c1c228aa4f85a6cad1f
deniloss <danil.sitnik.work@gmail.com>
2022-07-26 17:18:45 +0300

    .

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,6 @@ import Promo from "./Promo/Promo";
 import Navigation from "../Navigation/Navigation";
 import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
-import AboutMe from "./AboutMe/AboutMe";
 import Portfolio from "./Portfolio/Portfolio";
 import Footer from "../Footer/Footer";
 
@@ -32,7 +31,6 @@ const Main = ({loggedIn}) => {
       <Promo/>
       <AboutProject/>
       <Techs/>
-      <AboutMe/>
       <Portfolio/>
       <Footer/>
     </section>
